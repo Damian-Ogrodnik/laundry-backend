7683f1ebd6c9f74fe8dae12c6f8c01ad14779967
dogrodnik <d.ogrodnik96@gmail.com>
2020-02-10 16:21:53 +0100

    Return user details from profile route

--- src/controllers/user/userProfile.js
+++ src/controllers/user/userProfile.js
@@ -1,6 +1,10 @@
+const User = require("../../modals/User");
+
 async function userProfile(req, res, next) {
   try {
-    res.status(201).send("Profile");
+    let user = await await User.findById(req.user);
+    user.password = null;
+    res.status(201).json({ user });
   } catch (err) {
     res.status(500).json({ msg: err.message });
     next(err);
