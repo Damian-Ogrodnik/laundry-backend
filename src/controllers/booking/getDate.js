19f6148b6f950b3deafc61c04921929ce858adb3
dogrodnik <d.ogrodnik96@gmail.com>
2020-02-15 11:35:18 +0100

    Maintain respond

--- src/controllers/booking/getDate.js
+++ src/controllers/booking/getDate.js
@@ -2,9 +2,14 @@ const Booking = require("../../modals/Booking");
 
 async function getDate(req, res, next) {
   try {
-    console.log(req.body.date);
-    console.log(req.user);
-    res.status(201).json("Booking");
+    let booking = await Booking.findOne({ date: req.body.date });
+
+    if (!booking) {
+      booking = new Booking(req.body);
+      await booking.save();
+      return res.status(200).json({ booking });
+    }
+    res.status(200).json({ booking });
   } catch (err) {
     res.status(500).json({ msg: err.message });
     next(err);
