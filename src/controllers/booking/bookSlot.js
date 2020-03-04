e6bf4287115816f5a65de7c8995cdb2c19e33f1f
dogrodnik <d.ogrodnik96@gmail.com>
2020-03-04 09:14:30 +0100

    Fix bug with duble bookings

--- src/controllers/booking/bookSlot.js
+++ src/controllers/booking/bookSlot.js
@@ -3,6 +3,7 @@ const User = require("../../modals/User");
 
 async function bookSlot(req, res, next) {
   try {
+    let bookingAvaiable = true;
     let booking = await Booking.findOne({ date: req.body.date });
     let user = await User.findById(req.user);
 
@@ -20,23 +21,27 @@ async function bookSlot(req, res, next) {
 
     booking.slots.forEach(slot => {
       if (String(slot.user) === req.user) {
+        bookingAvaiable = false;
         return res
           .status(400)
           .json({ msg: "You can not book two slots for the one day" });
       }
       if (slot.number === req.body.number) {
+        bookingAvaiable = false;
         return res.status(400).json({ msg: "Slot is taken" });
       }
     });
 
-    await booking.slots.push(slot);
-    await booking.save();
-    if (!user.slots) {
-      user.slots = [];
+    if (bookingAvaiable) {
+      await booking.slots.push(slot);
+      await booking.save();
+      if (!user.slots) {
+        user.slots = [];
+      }
+      await user.slots.push(slot);
+      await user.save();
+      return res.status(200).json({ slot });
     }
-    await user.slots.push(slot);
-    await user.save();
-    return res.status(200).json({ slot });
   } catch (err) {
     return res.status(500).json({ msg: err.message });
   }
