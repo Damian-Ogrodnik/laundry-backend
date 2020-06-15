74af5dc1f0d35df0156e3dd6bf8b60b2b13cb7c4
dogrodnik <d.ogrodnik96@gmail.com>
2020-06-15 10:00:55 +0200

    Change password route bug  fix

--- src/controllers/admin/changeUserPassword.js
+++ src/controllers/admin/changeUserPassword.js
@@ -3,8 +3,8 @@ const bcrypt = require("bcryptjs");
 
 async function changeUserPassword(req, res, next) {
   try {
-    let user = await User.findById(req.query.id);
-    user.password = req.query.newPassword;
+    let user = await User.findById(req.body.id);
+    user.password = req.body.newPassword;
     await user.save();
     res.status(200).json({ msg: "User password changed" });
   } catch (err) {
