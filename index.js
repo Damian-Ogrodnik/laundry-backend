844418ff596a670cc9391593f77cb30472f3593a
dogrodnik <d.ogrodnik96@gmail.com>
2020-06-17 09:50:18 +0200

    Port settings change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const config = require("./src/config/config");
 const app = require("./src/server");
 require("./src/database/database");
 
-const PORT = 7000;
+const PORT = process.env.PORT || config.PORT;
 
 app.listen(PORT, () => {
   console.log("Server is listening...");
